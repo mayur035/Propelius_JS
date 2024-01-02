060fd5d6bc00788d87c6a13e898470caece15d15
mayur035 <mgp312002@gmail.com>
2024-01-02 17:57:11 +0530

    till Ch2_009 Function Invocation and the Execution Stack

---
+++ object.js
@@ -0,0 +1,52 @@
+// address={
+//     Street:'main',
+//     number:12234321,
+//     Apartment:{
+//         floor:3,
+//         number:301
+//     }
+// }
+
+// global object
+
+// var a 
+// console.log(a);
+
+// function b(){
+//     var xyz="how are you?"
+// }
+
+// a= 'heeloo'
+
+
+//if any function is inside the object then we say that function method... method->obj
+//normal function->global object(window, global)
+
+// const a={
+//     title:'abc',
+//     play(){
+//         console.log(this)
+//     }
+// }
+
+// function Video(title){
+//     this.title = title
+//     console.log(this);
+// }
+
+// const v=new Video('a')
+
+
+
+function b(){
+    console.log("This is function B");
+}
+
+b();
+
+console.log(a);
+
+var a="this is variable A"
+
+console.log(a);
+
